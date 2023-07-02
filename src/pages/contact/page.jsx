41dddeb17e4eb8fa2f4385713fdcd33f4a432c38
SatelliteSilver <42998574+SatelliteSilver@users.noreply.github.com>
2023-07-02 15:32:51 +0900

    Delete src directory

--- src/pages/contact/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ContactPage() {
-  return (
-    <h1>Contact</h1>
-  );
-}
--- src/pages/start/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function StartPage() {
-  return (
-    <h1>Start</h1>
-  );
-}
